87e73acf28b231f617b284e8239036922b032e8a
cantgetin <84032933+cantgetin@users.noreply.github.com>
2023-03-24 00:28:52 +0500

    OpenLayers Control components

---
+++ src/components/OpenLayers/Controls/FullScreenControl.tsx
@@ -0,0 +1,17 @@
+import { useContext, useEffect } from "react";
+import { FullScreen } from "ol/control";
+import MapContext from "../Map/MapContext";
+
+const FullScreenControl = () => {
+    const map  = useContext(MapContext);
+
+    useEffect(() => {
+        if (!map) return;
+        let fullScreenControl = new FullScreen({});
+        map.addControl(fullScreenControl)
+    }, [map]);
+
+    return null;
+};
+
+export default FullScreenControl;
